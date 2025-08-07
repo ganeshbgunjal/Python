b0db983793f43857d611e08cb91f2a6060b7c784
ganeshgunjal118@gmail.com <ganeshgunjal118@gmail.com>
2025-08-07 11:43:30 +0530

    folder add

---
+++ 5 importing and Creating Modules and Packages/package/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,5 @@
+'''
+This __init__.py is a python file that that defines packages and thei namespaces.
+This file maybe empty but it should be created to initialise this package folder.
+so this is very important folder.
+'''
