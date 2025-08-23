8d3cb79633630df0267e641125f27a2220698a7d
ganeshgunjal118@gmail.com <ganeshgunjal118@gmail.com>
2025-08-23 16:51:14 +0530

    file add

---
+++ 12 MultiThreading and Multiprocessing/4 webscrapping usecase.py
@@ -0,0 +1,28 @@
+import threading
+import requests
+
+from bs4 import BeautifulSoup   #this is beautifulsoup 4 library. 
+urls = [
+        'https://python.langchain.com/docs/tutorials/',
+
+        'https://python.langchain.com/docs/how_to/',
+
+        'https://python.langchain.com/docs/concepts/'
+]
+
+def fetch_content(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content,'html.parser')
+    print(f'Fetched {len(soup.text)} from {url}')
+
+threads = []
+
+for url in urls:
+    thread = threading.Thread(target=fetch_content,args=(url,))
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+print('All Web pages fetched!')
